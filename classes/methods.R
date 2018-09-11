9d59bbd1fc6cb8fe82b029dc5385bce27b481ac7
Luiz Rodrigo de Souza <me@lurodrigo.com>
2018-09-11 20:30:18 -0300

    Methods

--- classes/methods.R
+++ classes/methods.R
@@ -16,22 +16,6 @@ euler = function(f, y0, a, b, h) {
   m
 }
 
-tb = euler(function(t, u) (u - t)/(u + t), c("u" = 1), 0, 7, 0.1)
-ggplot(tb %>% as.data.frame(), aes(x = t, y = u)) + geom_path()
-
-sigma = 10
-rho = 27
-beta = 8/3
-tb = euler(function(t, y) c(sigma*(y[2]- y[1]), 
-                            y[1]*(rho - y[3]) - y[2], 
-                            y[1]*y[2] - beta*y[3]), 
-           c(1, 0, 0), 0, 30, 0.005) %>% as.data.frame()
-
-ggplot(tb %>% as.data.frame(), aes(x = t, y = y1)) + geom_path()
-ggplot(tb %>% as.data.frame(), aes(x = y2, y = y1)) + geom_path()
-
-## aula 5
-
 fixedPoint = function(f, x0, tol = 1e-10, steps = 20) {
   while (TRUE) {
     xn = f(x0)
@@ -85,27 +69,3 @@ huen = function(f, y0, a, b, h) {
   colnames(m) = c("t", if (is.null(names(y0))) paste0("y", 1:length(y0)) else names(y0))
   m
 }
-
-tb = euler(function(t, u) (u - t)/(u + t), c("u" = 1), 0, 7, 0.1)
-ggplot(tb %>% as.data.frame(), aes(x = t, y = u)) + geom_path()
-
-tb = bwEuler(function(t, u) (u - t)/(u + t), c("u" = 1), 0, 7, 0.1)
-ggplot(tb %>% as.data.frame(), aes(x = t, y = u)) + geom_path()
-
-tb = crankNicolson(function(t, u) (u - t)/(u + t), c("u" = 1), 0, 7, 0.1)
-ggplot(tb %>% as.data.frame(), aes(x = t, y = u)) + geom_path()
-
-tb = huen(function(t, u) (u - t)/(u + t), c("u" = 1), 0, 7, 0.1)
-ggplot(tb %>% as.data.frame(), aes(x = t, y = u)) + geom_path()
-
-####
-arg = list(function(t, u) -5*u, c("u" = 1), 0, 1, 0.025)
-tb = invoke_map(list(euler, bwEuler, crankNicolson, huen), 
-                  list(arg, arg, arg, arg)) %>%
-  map(as.data.frame) %>%
-  bind_cols %>%
-  select(t, mt_euler = u, mt_bwEuler = u1, mt_crankNicolson = u2, mt_huen = u3) %>%
-  mutate(u = exp(-5*t)) %>%
-  tail(1)
-
-abs((tb %>% select(starts_with("mt")) %>% as.matrix) - tb$u[nrow(tb)]) 
